b26e8f62ab83bbe40b4a7fb5a91ade90b3cd627a
itment <100358823+poiko34@users.noreply.github.com>
2025-03-23 02:44:31 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
 
-# Устанавливаем python3-venv и создаем виртуальное окружение
-apt install python3-venv -y
-python3 -m venv path/to/venv
-python3 -m venv myenv
+# Устанавливаем mailtm
+sudo pip3 install mailtm --break-system-packages
 
-# Активируем виртуальное окружение
-source myenv/bin/activate
-
-# Устанавливаем пакет MailTm через pip
-pip3 install MailTm
-
-# Устанавливаем pipx для установки Python приложений
-apt install pipx -y
-pipx install MailTm
-
-# Устанавливаем python3-xyz через apt
-apt install python3-xyz -y
-
-# Устанавливаем MailTm через pip с флагом --break-system-packages
-pip install MailTm --break-system-packages
 cp mail.py /
 # Исправляем символы возврата каретки (\r) в mail.py (если они есть)
 dos2unix /mail.py
